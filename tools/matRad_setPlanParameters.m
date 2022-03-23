38d359fbfc656d51ae063365aafc3ab7960ee1c4
Noa Homolka <n.homolka@dkfz.de>
2022-03-23 14:56:00 +0100

    Update matRad_setPlanParameters.m
    added testing flag

--- tools/matRad_setPlanParameters.m
+++ tools/matRad_setPlanParameters.m
@@ -1,11 +1,37 @@
-function pln = matRad_setPlanParameters(ct,cst,baseData,RBE_model)
-pln.propStf.gantryAngles = [40,340,300];
-pln.propStf.couchAngles = [0,0,0];
-pln.propStf.bixelWidth = 3;
+function [pln,cst] = matRad_setPlanParameters(ct,cst,baseData,RBE_model,testing,pln)
+if ~exist('pln','var')
+    pln = struct;
+end
+if nargin < 5
+    testing = false;
+end
+if testing
+    if ~isfield(pln,'propStf') || ~isfield(pln.propStf,'couchAngles')
+        pln.propStf.gantryAngles = 0;
+        pln.propStf.couchAngles = 0;
+    end
+    pln.propStf.bixelWidth = 5;
+else
+    pln.propStf.bixelWidth = 3;
+end
 pln.propStf.numOfBeams = size(pln.propStf.gantryAngles,2);
 pln.propStf.isoCenter = ones(pln.propStf.numOfBeams,1)*matRad_getIsoCenter(cst,ct);
 
-pln.numOfFractions = 60;
+pln.numOfFractions = 30;
+
+target = find(contains(cst(:,3),'TARGET'));
+if ~iscell(cst{target(1),6})
+    cst = matRad_computeVoiContoursWrapper(cst,ct);
+end
+if ~iscell(cst{target(1),6})
+    cst = matRad_convertOldCstToNewCstObjectives(cst);
+end
+for i = target'
+    cst{i,6}{1}.parameters{1} = 60;
+end
+if size(cst,2)>6 && iscell(cst{1,7})
+    cst(:,7)=[];
+end
 
 s = split(baseData,'_');
 pln.radiationMode   = s{1};
@@ -27,15 +53,26 @@ pln.propDoseCalc.calcLET = 0;
 % pln = rmfield(pln,{'bioParam','multScen','DicomInfo'});
 pln.bioParam = matRad_bioModel(pln.radiationMode,quantityOpt,RBE_model);
 pln.multScen = matRad_multScen(ct,'nomScen'); % optimize on the nominal scenario
-pln.propDoseCalc.resolution = struct('x',1,'y',1,'z',1);
+if testing
+    pln.propDoseCalc.resolution = struct('x',3,'y',3,'z',3);
+else
+    pln.propDoseCalc.resolution = struct('x',3,'y',3,'z',3);
+end
 
 pln.propHeterogeneity.calcHetero = true;
 pln.propOpt.runDAO        = 0;
 pln.propOpt.runSequencing = 0;
 
 % Monte Carlo settings
-pln.propMC.numOfRuns = 5;
+if testing
+    pln.propMC.histories = 1e4;
+    pln.propMC.numOfRuns = 1;
+    pln.propMC.externalCalculation = false;
+else
+    pln.propMC.histories = 1e8;
+    pln.propMC.numOfRuns = 5;
+    pln.propMC.externalCalculation = true;
+end
 pln.propMC.engine = 'TOPAS';
-pln.propMC.histories = 1e8;
-pln.propMC.externalCalculation = true;
+
 end
