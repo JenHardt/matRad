d0c82ea2afcf1b9bff6b479ff94fda723c07188f
Noa Homolka <n.homolka@dkfz.de>
2022-06-07 17:31:44 +0200

    Update matRad_TopasRecalcRBEfromLET.m
    EXPERIMENTAL: preliminary change to allow calculation of dij

--- topas/matRad_TopasRecalcRBEfromLET.m
+++ topas/matRad_TopasRecalcRBEfromLET.m
@@ -5,16 +5,28 @@ if nargin <=2
     betaX = 0.05;
 end
 
-numOfBeams = sum(contains(fieldnames(resultGUI),'LET_beam'));
-wBeam = resultGUI.w;
-doseGrid = size(resultGUI.LET);
+if isfield(resultGUI,'LET')
 
-for i = 1:numOfBeams
-    beamInfo(i).suffix = ['_beam', num2str(i)];
-    beamInfo(i).logIx  = ([1:numOfBeams]' == i);
+else
+    numOfBeams = sum(contains(fieldnames(resultGUI),'LET_beam'));
 end
-beamInfo(numOfBeams+1).suffix = '';
-beamInfo(numOfBeams+1).logIx  = true(size(resultGUI.w));
+
+if isfield(resultGUI,'w')
+    wBeam = resultGUI.w;
+else
+    wBeam = ones(resultGUI.totalNumOfBixels);
+end
+if isfield(resultGUI,'LET')
+    numOfVoxel = numel(resultGUI.LET);
+    numOfBeams = sum(contains(fieldnames(resultGUI),'LET_beam'));
+    doseGrid = size(resultGUI.LET);
+    dij = 0;
+elseif isfield(resultGUI,'mLETDose')
+    numOfVoxel = size(resultGUI.mLETDose{1},1);
+    numOfBeams = resultGUI.numOfBeams;
+    numOfBeamlets = size(resultGUI.mLETDose{1},2);
+    dij = 1;
+end
 
 ABratio = alphaX / betaX;
 
@@ -27,46 +39,83 @@ modelParam.p0_WED   = 1; % https://www.ncbi.nlm.nih.gov/pubmed/22909391
 modelParam.p1_WED   = 0.434;
 modelParam.p2_WED   = 1;
 
-numOfVoxel = numel(resultGUI.LET);
-mAlphaDose = spalloc(numOfVoxel,numOfBeams,1);
-mSqrtBetaDose = spalloc(numOfVoxel,numOfBeams,1);
+if ~dij
+    mAlphaDose = spalloc(numOfVoxel,numOfBeams,1);
+    mSqrtBetaDose = spalloc(numOfVoxel,numOfBeams,1);
 
-for i = 1:length(beamInfo)-1
-    LET = resultGUI.(['LET', beamInfo(i).suffix]);
-    LET(isnan(LET)) = 0;
+    for i = 1:numOfBeams
+        beamInfo(i).suffix = ['_beam', num2str(i)];
+        beamInfo(i).logIx  = ([1:numOfBeams]' == i);
+    end
+    beamInfo(numOfBeams+1).suffix = '';
+    beamInfo(numOfBeams+1).logIx  = true(numOfBeams);
 
-    switch modelName
-        case 'MCN'
+    for i = 1:length(beamInfo)-1
+        LET = resultGUI.(['LET', beamInfo(i).suffix]);
+        LET(isnan(LET)) = 0;
 
-            RBEmax     = modelParam.p0_MCN + ((modelParam.p1_MCN * LET )./ ABratio);
-            RBEmin     = modelParam.p2_MCN + (modelParam.p3_MCN  * sqrt(ABratio) .* LET);
+        switch modelName
+            case 'MCN'
 
-            resultGUI.(['alpha_recalc_MCN', beamInfo(i).suffix]) = RBEmax    .* alphaX;
-            resultGUI.(['beta_recalc_MCN', beamInfo(i).suffix])  = RBEmin.^2 .* betaX;
+                RBEmax     = modelParam.p0_MCN + ((modelParam.p1_MCN * LET )./ ABratio);
+                RBEmin     = modelParam.p2_MCN + (modelParam.p3_MCN  * sqrt(ABratio) .* LET);
 
-        case 'WED'
+                resultGUI.(['alpha_recalc_MCN', beamInfo(i).suffix]) = RBEmax    .* alphaX;
+                resultGUI.(['beta_recalc_MCN', beamInfo(i).suffix])  = RBEmin.^2 .* betaX;
 
-            RBEmax     = modelParam.p0_WED + ((modelParam.p1_WED * LET )./ ABratio);
-            RBEmin     = modelParam.p2_WED;
+            case 'WED'
 
-            resultGUI.(['alpha_recalc_WED', beamInfo(i).suffix]) = RBEmax    .* alphaX;
-            resultGUI.(['beta_recalc_WED', beamInfo(i).suffix])  = RBEmin.^2 .* betaX;
+                RBEmax     = modelParam.p0_WED + ((modelParam.p1_WED * LET )./ ABratio);
+                RBEmin     = modelParam.p2_WED;
+
+                resultGUI.(['alpha_recalc_WED', beamInfo(i).suffix]) = RBEmax    .* alphaX;
+                resultGUI.(['beta_recalc_WED', beamInfo(i).suffix])  = RBEmin.^2 .* betaX;
+
+        end
+
+        mAlphaDose(:,i)         = reshape(resultGUI.(['alpha_recalc_' modelName, beamInfo(i).suffix]) .* resultGUI.(['physicalDose', beamInfo(i).suffix]),[],1);
+        mSqrtBetaDose(:,i)      = reshape(sqrt(resultGUI.(['beta_recalc_' modelName, beamInfo(i).suffix])) .* resultGUI.(['physicalDose', beamInfo(i).suffix]),[],1);
 
     end
 
-    mAlphaDose(:,i)         = reshape(resultGUI.(['alpha_recalc_' modelName, beamInfo(i).suffix]) .* resultGUI.(['physicalDose', beamInfo(i).suffix]),[],1);
-    mSqrtBetaDose(:,i)      = reshape(sqrt(resultGUI.(['beta_recalc_' modelName, beamInfo(i).suffix])) .* resultGUI.(['physicalDose', beamInfo(i).suffix]),[],1);
+    for i = 1:length(beamInfo)
+        resultGUI.(['effect_recalc_' modelName, beamInfo(i).suffix])       = full(mAlphaDose * beamInfo(i).logIx + (mSqrtBetaDose * beamInfo(i).logIx).^2);
+        resultGUI.(['effect_recalc_' modelName, beamInfo(i).suffix])       = reshape(resultGUI.(['effect_recalc_' modelName, beamInfo(i).suffix]),doseGrid);
 
-end
+        resultGUI.(['RBExD_recalc_' modelName, beamInfo(i).suffix])        = zeros(size(resultGUI.(['effect_recalc_' modelName, beamInfo(i).suffix])));
+        resultGUI.(['RBExD_recalc_' modelName, beamInfo(i).suffix])        = (sqrt(alphaX.^2 + 4 .* betaX .* resultGUI.(['effect_recalc_' modelName, beamInfo(i).suffix])) - alphaX)./(2.*betaX);
 
-for i = 1:length(beamInfo)
-    resultGUI.(['effect_recalc_' modelName, beamInfo(i).suffix])       = full(mAlphaDose * beamInfo(i).logIx + (mSqrtBetaDose * beamInfo(i).logIx).^2);
-    resultGUI.(['effect_recalc_' modelName, beamInfo(i).suffix])       = reshape(resultGUI.(['effect_recalc_' modelName, beamInfo(i).suffix]),doseGrid);
+        resultGUI.(['RBE_recalc_' modelName, beamInfo(i).suffix])          = resultGUI.(['RBExD_recalc_' modelName, beamInfo(i).suffix])./resultGUI.(['physicalDose', beamInfo(i).suffix]);
+    end
 
-    resultGUI.(['RBExD_recalc_' modelName, beamInfo(i).suffix])        = zeros(size(resultGUI.(['effect_recalc_' modelName, beamInfo(i).suffix])));
-    resultGUI.(['RBExD_recalc_' modelName, beamInfo(i).suffix])        = (sqrt(alphaX.^2 + 4 .* betaX .* resultGUI.(['effect_recalc_' modelName, beamInfo(i).suffix])) - alphaX)./(2.*betaX);
+else
+%     resultGUI.mLETDose{1}(isnan(resultGUI.mLETDose{1})) = 0;
 
-    resultGUI.(['RBE_recalc_' modelName, beamInfo(i).suffix])          = resultGUI.(['RBExD_recalc_' modelName, beamInfo(i).suffix])./resultGUI.(['physicalDose', beamInfo(i).suffix]);
+    for beamLet = 1:numOfBeamlets
+        switch modelName
+            case 'MCN'
+
+                RBEmax     = modelParam.p0_MCN + ((modelParam.p1_MCN * full(resultGUI.mLETDose{1}(:,beamLet)) )./ ABratio);
+                RBEmin     = modelParam.p2_MCN + (modelParam.p3_MCN  * sqrt(ABratio) .* full(resultGUI.mLETDose{1}(:,beamLet)));
+
+                resultGUI.alpha_recalc{1}(:,beamLet) = RBEmax    .* alphaX;
+                resultGUI.beta_recalc{1}(:,beamLet)  = RBEmin.^2 .* betaX;
+
+            case 'WED'
+
+                RBEmax     = modelParam.p0_WED + ((modelParam.p1_WED * full(resultGUI.mLETDose{1}(:,beamLet)) )./ ABratio);
+                RBEmin     = modelParam.p2_WED;
+
+                resultGUI.alpha_recalc{1}(:,beamLet) = RBEmax    .* alphaX;
+                resultGUI.beta_recalc{1}(:,beamLet)  = RBEmin.^2 .* betaX;
+
+        end
+    end
+
+    resultGUI.mAlphaDose_recalc{1}      = resultGUI.alpha_recalc{1} .* resultGUI.physicalDose{1};
+    resultGUI.mSqrtBetaDose_recalc{1}   = sqrt(resultGUI.beta_recalc{1}) .* resultGUI.physicalDose{1};
+
+    resultGUI = rmfield(resultGUI,{'alpha_recalc','beta_recalc'});
 end
 
 resultGUI = orderfields(resultGUI);
